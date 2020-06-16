e24fadbcda3f9a7eae1b504371b084df0875384f
Meru Patel <merupatel123@gmail.com>
2020-06-16 13:55:55 +0530

    Update new_script.gd

--- new_script.gd
+++ new_script.gd
@@ -8,5 +8,5 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
