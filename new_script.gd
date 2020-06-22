34f1dc3d729512a48083327b8a820d97e926db97
janglee <merupatel123@gmail.com>
2020-06-22 14:55:00 +0530

    A too nice commit

--- new_script.gd
+++ new_script.gd
@@ -5,10 +5,6 @@ extends Node
 # var b = "text"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# local change
-# remote commit
-
+	pass 
 func _process(delta):
 	pass
