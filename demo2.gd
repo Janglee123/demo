b50bd5bb0c46c9f69befd7f37b647cc35944c70d
janglee <merupatel123@gmail.com>
2020-07-24 14:36:27 +0530

    A good commit

--- demo2.gd
+++ demo2.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+onready var child := $Child as Node2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -7,4 +9,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
