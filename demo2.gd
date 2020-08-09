868bcb72527be1013afcce1fdd5b25e9a1036dc0
janglee <merupatel123@gmail.com>
2020-08-09 11:21:53 +0530

    Add a commit message

--- demo2.gd
+++ demo2.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+const MIN_TIME = 2
 onready var child := $Child as Node2D
+onready var time := 0
 
-var time := 0.0
 var max_time := 5.0
+
+func _ready():
+	max_time /= 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time += delta
-	if time >= max_time:
-		time = 0
+	if time >= 2*max_time:
+		time = MIN_TIME
